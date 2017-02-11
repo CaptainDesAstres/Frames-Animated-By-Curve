88ac54d7ba9b8cb90bf63645cd2abe01801631bd
Captain DesAstres <captaindesastres@gmail.com>
2017-02-11 09:09:06 +0100

    add basic register and unregister fonction

--- __init__.py
+++ __init__.py
@@ -1,1 +1,18 @@
- 
+bl_info = {
+    "name": "Frames Animated By Curve",
+    "author": "Captain DesAstres",
+    "version": (0, 1),
+    "blender": (2, 78, 0),
+    "location": "Movie Clip Editor -> Tools",
+    "description": "A Blender plugin who take a short animation of a simple move to make a bigger one, using a F-Curve.",
+    "wiki_url": "https://github.com/CaptainDesAstres/Frames-Animated-By-Curve",
+    "category": "Animation"}
+
+
+import bpy
+
+def register():
+	print("Frames Animated By Curve is enable")
+
+def unregister():
+	print("Frames Animated By Curve is disable")
