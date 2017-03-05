08285548bf7cf426387bfd0afd0d47bdb7739ff1
Captain DesAstres <captaindesastres@gmail.com>
2017-03-05 13:28:46 +0100

    avoid 0 division error

--- __init__.py
+++ __init__.py
@@ -224,14 +224,17 @@ def set_ppm(self, context):
 				curve.keyframe_points[-1].interpolation = 'LINEAR'
 				
 				# next frame
-				interval = 60 / ppm.evaluate(frame) * fps / 2
-				frame += interval
-				
-				# invert value
-				if value == 0:
-					value = 1
+				if(ppm.evaluate(frame) > 0):
+					interval = 60 / ppm.evaluate(frame) * fps / 2
+					frame += interval
+					
+					# invert value
+					if value == 0:
+						value = 1
+					else:
+						value = 0
 				else:
-					value = 0
+					break
 		curve.lock = True
 
 
