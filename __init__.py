7b867e71d96607248d9f0f9a698c1010dd05fbe3
Captain DesAstres <captaindesastres@gmail.com>
2017-03-17 12:00:59 +0100

    split peaks interpolation top settings drawing into a dedicated method

--- __init__.py
+++ __init__.py
@@ -1385,6 +1385,58 @@ class CtF(bpy.types.PropertyGroup):
 	
 	
 	
+	def draw_top_interpolation( self, layout ):
+		'''draw peaks interpolation settings (top) into the panel'''
+		# top peak interpolation same as main
+		row = layout.row()
+		col = row.column()
+		col.prop(self, "top_interpolation_same", text = 'Same')
+		
+		if not self.top_interpolation_same:
+			# keyframes interpolation mode
+			col = row.column()
+			col.prop(self, "top_interpolation", text = '')
+			
+			# easing mode
+			col = row.column()
+			col.prop(self, "top_easing", text = '')
+			if(self.top_interpolation in [
+							'linear',
+							'bezier auto',
+							'bezier free'] 
+							):
+				col.enabled = False
+			
+			# Right auto
+			col = row.column()
+			col.prop(self, "top_right_auto")
+			if (self.top_interpolation != 'bezier free'):
+				col.enabled = False
+			
+			if (self.top_interpolation == 'bezier free'):
+				# Left length
+				row = layout.row()
+				col = row.column()
+				col.prop(self, "top_left_length")
+				
+				# left angle
+				col = row.column()
+				col.prop(self, "top_left_angle")
+				
+				if not self.top_right_auto:
+					# Right length
+					row = layout.row()
+					col = row.column()
+					col.prop(self, "top_right_length")
+					
+					# Right angle
+					col = row.column()
+					col.prop(self, "top_right_angle")
+	
+	
+	
+	
+	
 	
 	
 	def draw(self, context, layout, clip):
@@ -1407,56 +1459,9 @@ class CtF(bpy.types.PropertyGroup):
 			# draw peaks interpolation main settings
 			self.draw_main_interpolation( layout )
 			
+			# draw peaks interpolation top settings
+			self.draw_top_interpolation( layout )
 			
-			
-			##########################################
-			##    top peaks interpolation settings  ##
-			##########################################
-			# top peak interpolation same as main
-			row = layout.row()
-			col = row.column()
-			col.prop(self, "top_interpolation_same", text = 'Same')
-			
-			if not self.top_interpolation_same:
-				# keyframes interpolation mode
-				col = row.column()
-				col.prop(self, "top_interpolation", text = '')
-				
-				# easing mode
-				col = row.column()
-				col.prop(self, "top_easing", text = '')
-				if(self.top_interpolation in [
-								'linear',
-								'bezier auto',
-								'bezier free'] 
-								):
-					col.enabled = False
-				
-				# Right auto
-				col = row.column()
-				col.prop(self, "top_right_auto")
-				if (self.top_interpolation != 'bezier free'):
-					col.enabled = False
-				
-				if (self.top_interpolation == 'bezier free'):
-					# Left length
-					row = layout.row()
-					col = row.column()
-					col.prop(self, "top_left_length")
-					
-					# left angle
-					col = row.column()
-					col.prop(self, "top_left_angle")
-					
-					if not self.top_right_auto:
-						# Right length
-						row = layout.row()
-						col = row.column()
-						col.prop(self, "top_right_length")
-						
-						# Right angle
-						col = row.column()
-						col.prop(self, "top_right_angle")
 			
 			
 			
