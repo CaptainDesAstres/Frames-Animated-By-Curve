cee6f5e55f8b440f503e4ff3bbfda3639d385a41
Captain DesAstres <captaindesastres@gmail.com>
2017-05-31 13:40:07 +0200

    copy update_curves method in MultiTrack class

--- multi_track/MultiTrack.py
+++ multi_track/MultiTrack.py
@@ -17,4 +17,39 @@ class MultiTrack(bpy.types.PropertyGroup, Peaks, CurveToFrameProperty):
 			Properties, methods and operators 
 			for multi track feature'''
 	
-	
+	def update_curves( self, context ):
+		'''update curve when settings have been changed'''
+		# initialize animation data if required
+		if self.id_data.animation_data is None:
+			self.id_data.animation_data_create()
+		
+		if self.id_data.animation_data.action is None:
+			self.id_data.animation_data.action = bpy.data.actions.new( 
+										name= self.id_data.name+'Action')
+		
+		# check and get peaks shapes
+		peak_shapes = self.check_and_get_peaks_shapes()
+		if type(peak_shapes) is str:
+			return peak_shapes
+		
+		# update amplitude net curve
+		amplitude_net_curve = self.update_net_amplitude_curve( self.id_data, context )
+		
+		
+		# update peaks curve
+		peaks_curve = self.update_peaks_curve(self.id_data, context,
+							amplitude_net_curve, peak_shapes )
+		
+		#update combination curve
+		combination_curve = self.update_combination_curve(
+												self.id_data, 
+												context, 
+												amplitude_net_curve,
+												peaks_curve
+												)
+		
+		if(type(self.id_data) is bpy.types.MovieClip ):
+			# update output curve
+			self.update_output_curve(self.id_data, context, combination_curve)
+		
+		return True
