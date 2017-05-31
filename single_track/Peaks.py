d753c6e6db1ed126a48c6ee5e3e2c8b3ad2f1322
Captain DesAstres <captaindesastres@gmail.com>
2017-05-31 12:47:12 +0200

    create seperated file and class to split code relative to peaks feature

---
+++ single_track/Peaks.py
@@ -0,0 +1,10 @@
+
+
+
+
+class Peaks():
+	'''A class containing all properties and methods
+		associates with peaks feature of 
+		Curve To Frame addon'''
+	
+	
--- single_track/SingleTrack.py
+++ single_track/SingleTrack.py
@@ -1,7 +1,8 @@
+from .Peaks import Peaks
 from .CurveToFrameProperty import CurveToFrameProperty
 import bpy
 
-class SingleTrack(bpy.types.PropertyGroup, CurveToFrameProperty):
+class SingleTrack(bpy.types.PropertyGroup, Peaks, CurveToFrameProperty):
 	''' class containing all Curve to frame 
 			Properties, methods and operators 
 			for single track feature'''
