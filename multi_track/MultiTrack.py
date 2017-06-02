c257567cab07f558a5a60afe7f5d9b71d27837ba
Captain DesAstres <captaindesastres@gmail.com>
2017-06-02 11:43:25 +0200

    split code into a class dedicated to tracks list methods and properties

--- multi_track/MultiTrack.py
+++ multi_track/MultiTrack.py
@@ -13,66 +13,10 @@ from uuid import uuid4
 
 
 
-class MultiTrack(bpy.types.PropertyGroup, Peaks, CurveToFrameProperty):
+class MultiTrack(bpy.types.PropertyGroup, TracksList, Peaks, CurveToFrameProperty):
 	''' class containing all Curve to frame 
 			Properties, methods and operators 
 			for multi track feature'''
-	
-	
-	def add_track( self, context ):
-		'''add the selected tracks in tracks list'''
-		# get new track name and avoid recursive call
-		track = self.track_add
-		if track == '':
-			return
-		
-		# get the corresponding movieclip
-		try:
-			track = bpy.data.movieclips[ track ]
-		except KeyError:
-			return
-		
-		# check the source is compatible
-		if track.source != 'SEQUENCE':
-			return
-		
-		# load tracks if necessary
-		if track.curve_to_frame.uid == '':
-			track.curve_to_frame.initialize()
-		if get_fcurve_by_data_path(track, 'curve_to_frame.peaks_shape') is None:
-			track.curve_to_frame.init_peaks_shape_curve()
-		
-		# add to the list
-		new = self.tracks.add()
-		new.name = track.name
-		new.uid = track.curve_to_frame.uid
-		new.track_id = len(self.tracks)-1
-		self.selected_track = new.track_id
-		
-		# clear the add field
-		self.track_add=''
-	
-	
-	
-	
-	#########################
-	## list and properties ##
-	#########################
-	
-	track_add = bpy.props.StringProperty(
-				name = "Add",
-				description = "Add tracks to the list",
-				default = '',
-				update = add_track )
-	
-	tracks = bpy.props.CollectionProperty(
-				type=Track,
-				options = {'LIBRARY_EDITABLE'} )
-	
-	selected_track = bpy.props.IntProperty( default = -1 )
-	
-	
-	
 	
 	
 	def update_curves( self, context ):
