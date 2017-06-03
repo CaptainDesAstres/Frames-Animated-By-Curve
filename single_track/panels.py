88a04c1727e32a676beb2308a1c26ab91dd349d1
Captain DesAstres <captaindesastres@gmail.com>
2017-06-03 12:29:37 +0200

    put amplitude panel drawing method into Panel class

--- single_track/panels.py
+++ single_track/panels.py
@@ -1,4 +1,5 @@
 import bpy
+from functions import *
 
 
 class TrackPanel(bpy.types.Panel):
@@ -35,6 +36,60 @@ class TrackPanel(bpy.types.Panel):
 
 class Panel():
 	'''class containing all needed method to draw panel'''
+	
+	def draw_amplitude( 
+						self,
+						layout, 
+						refresh_curve, 
+						refresh_mini_maxi):
+		'''draw amplitude settings into the panel'''
+		# A float amplitude field
+		layout.separator()
+		row = layout.row()
+		col = row.column()
+		col.prop(self, "amplitude")
+		
+		# A field to remind F-Curve min and max value
+		fCurve = get_fcurve_by_data_path(self.id_data, 'curve_to_frame.amplitude')
+		if(fCurve is None):
+			m = M = self.amplitude
+		else:
+			m, M = get_curve_limit(fCurve)
+		m = round(m*1000)/1000
+		M = round(M*1000)/1000
+		col = row.column()
+		col.label( text = "(Goes from "+str(m)\
+					+" to "+str(M)+')' )
+		
+		# A field to set the min F-Curve Value to 
+		# assigne to the first frames
+		row = layout.row()
+		col = row.column()
+		col.prop(self, "mini")
+		
+		# A field to set the max F-Curve Value to 
+		# assigne to the last frames
+		col = row.column()
+		col.prop(self, "maxi")
+		if(self.combination_mode == 'ignore_amplitude'):
+			col.enabled = False
+		
+		# A button to get curve min max value
+		col = row.column()
+		col.operator(refresh_mini_maxi,
+					text = 'auto')
+		# display net amplitude value
+		col = row.column()
+		col.enabled = False
+		col.prop(self, "amplitude_net")
+		col = row.column()
+		col.operator(
+			refresh_curve,
+			text='',
+			icon='FILE_REFRESH')
+	
+	
+	
 	
 
 
