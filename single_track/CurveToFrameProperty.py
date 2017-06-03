d26115ca24076c81b7c602a2719db21c73196520
Captain DesAstres <captaindesastres@gmail.com>
2017-06-03 15:02:00 +0200

    move property and method to Combination class

--- single_track/CurveToFrameProperty.py
+++ single_track/CurveToFrameProperty.py
@@ -24,105 +24,6 @@ class CurveToFrameProperty():
 	
 	
 	
-	def update_combination_curve(
-						self,
-						clip, 
-						context, 
-						amplitude_net_curve,
-						peaks_curve):
-		'''update clip combination curve'''
-		# get combination mode curve
-		combination_enum = clip.curve_to_frame.bl_rna.\
-													properties['combination_mode'].enum_items
-		combination_mode = combination_enum.find( clip.curve_to_frame.combination_mode )
-		combination_mode_curve = get_fcurve_by_data_path(clip, 
-								'curve_to_frame.combination_mode')
-		
-		# get and initialize combination curve
-		combination_curve = get_fcurve_by_data_path(clip, 
-								'curve_to_frame.combination')
-		if combination_curve is not None:
-			hide = combination_curve.hide
-			clip.animation_data.action.fcurves.remove(combination_curve)
-		else:
-			hide = True
-		clip.animation_data.action.fcurves.new(
-									'curve_to_frame.combination')
-		combination_curve = get_fcurve_by_data_path(clip, 
-									'curve_to_frame.combination')
-		
-		# get rate curve
-		rate_curve = get_fcurve_by_data_path(clip, 'curve_to_frame.rate')
-		
-		# loop only on peak curve keyframe
-		for keyframe in peaks_curve.keyframe_points:
-			# get peaks keyframe value and frame
-			frame = keyframe.co[0]
-			value = max( min(1, keyframe.co[1]), 0 )
-			
-			# get combination_mode at this frame
-			if combination_mode_curve is not None:
-				combination_mode = combination_mode_curve.evaluate(frame)
-			
-			# generate keyframe
-			if combination_mode != 3 : # «combination mode == multiply or clamp
-				value = value * amplitude_net_curve.evaluate(frame)
-			
-			if combination_mode != 4 :
-				combination_curve.keyframe_points.insert(frame, value)
-				combination_curve.keyframe_points[-1].interpolation = 'LINEAR'
-		
-		
-		# loop for all frame
-		end = max(	peaks_curve.keyframe_points[-1].co[0], 
-					context.scene.frame_end )
-		frame = start = context.scene.frame_start
-		while frame <= end:
-			# get combination_mode at this frame
-			if combination_mode_curve is not None:
-				combination_mode = combination_mode_curve.evaluate(frame)
-			
-			if combination_mode == 0 : # combination mode is «multiply»
-				value = max( min( 1, peaks_curve.evaluate(frame) ), 0 )\
-						* amplitude_net_curve.evaluate(frame)
-				combination_curve.keyframe_points.insert(frame, value)
-			elif combination_mode == 2: # combination mode is «clamp_curve»
-				combination_curve.keyframe_points.insert(
-						frame,
-						max(
-							min (
-									amplitude_net_curve.evaluate(frame),
-									peaks_curve.evaluate(frame),
-									1
-									),
-							0
-						)
-					)
-			elif combination_mode == 4: 
-				# combination mode is «ignore peaks»
-				combination_curve.keyframe_points.insert(
-							frame,
-							amplitude_net_curve.evaluate(frame)
-							)
-			
-			combination_curve.keyframe_points[-1].interpolation = 'LINEAR'
-			
-			# next frame
-			frame += 1
-		
-		#erase keyframe on flat section
-		avoid_useless_keyframe( combination_curve )
-		
-		# prevent curve edition
-		combination_curve.lock = True
-		combination_curve.hide = hide
-		
-		return combination_curve
-	
-	
-	
-	
-	
 	def update_output_curve(self, clip, context, combination_curve):
 		'''update output curve'''
 		# get and initialize output curve
@@ -213,15 +114,7 @@ class CurveToFrameProperty():
 	#################################################
 	##     output settings                         ##
 	#################################################
-	# combination of net amplitude and peaks curves
-	combination = bpy.props.FloatProperty(
-		name = "combination",
-		description = "Only to visualize the combination of \
-					peaks and amplitude curve curve. Can't \
-					be edit manually: use rate and amplitude settings.",
-		default = 0,
-		min = 0,
-		max = 1)
+	
 	
 	# output frame curve
 	output = bpy.props.IntProperty(
