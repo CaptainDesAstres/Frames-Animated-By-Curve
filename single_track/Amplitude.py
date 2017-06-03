5832bc30dd925601995e82d52bab8e9601951fbf
Captain DesAstres <captaindesastres@gmail.com>
2017-06-03 14:27:18 +0200

    move properties and methods to Amplitude class

--- single_track/Amplitude.py
+++ single_track/Amplitude.py
@@ -1,8 +1,103 @@
-
+import bpy
 
 
 class Amplitude():
 	'''A class containing all properties and methods
 		associates with amplitude feature of 
 		Curve To Frame addon'''
+	
+	def update_curves( self, context ):
+		'''method that must be over ride: update curve when settings have been changed'''
+	
+	
+	
+	
+	
+	def set_mini(self, context):
+		'''check that maxi value are greater than maxi value 
+				when editing mini value'''
+		if self.mini > self.maxi:
+			self['maxi'] = self.mini
+	
+	
+	
+	
+	
+	def set_maxi(self, context):
+		'''check that maxi value are greater than maxi value
+				when editing maxi value'''
+		if self.mini > self.maxi:
+			self['mini'] = self.maxi
+	
+	
+	
+	
+	#################################################
+	##     amplitude settings                      ##
+	#################################################
+	# amplitude property
+	amplitude = bpy.props.FloatProperty(
+		name = 'amplitude (raw)',
+		description = 'Determined the frame of the Movie \
+								clip to use at each frame',
+		default = 0.0
+		)
+	
+	# amplitude after applying min and max value
+	amplitude_net = bpy.props.FloatProperty(
+		name = 'amplitude (net)',
+		description = 'show the apply of mini and maxi to \
+							amplitude raw. Can\'t be edit.',
+		)
+	
+	# method used to combine amplitude and peaks curve
+	combination_mode = bpy.props.EnumProperty(
+		name = 'combination mode',
+		description = 'the way to combine amplitude and peaks curve',
+		default = 'ignore_peaks',
+		items = [
+#			(identifier,			name,
+#				description, number)
+			
+			('multiply',		'Peaks Curve Multiplied by amplitude',
+				'peaks is multiplied by \
+				amplitude percentage of maxi',				0),
+			
+			('clamp_key',		'Peaks Keyframe Clamped to amplitude',
+				'peaks keyframe is clamped by amplitude',		1),
+			
+			('clamp_curve',		'Peaks Curve Clamped to amplitude',
+				'all peaks value is clamped by amplitude',		2),
+			
+			('ignore_amplitude',			'Only use peaks curve',
+				'Only use peaks curve',			3),
+			
+			('ignore_peaks',			'Only use amplitude curve',
+				'Only use amplitude curve',			4)
+			
+			],
+		update = update_curves
+		)
+	
+	# min value associated to the first frames
+	mini = bpy.props.FloatProperty(
+		name = 'Mini',
+		description = 'the minimal value of the \
+						amplitude curve, all smaller\
+						 value will display the first frame',
+		default = 0.0,
+		update = set_mini
+		)
+	
+	# max value associated to the last frames
+	maxi = bpy.props.FloatProperty(
+		name = 'Maxi',
+		description = 'the maximal value of the amplitude\
+						 curve. All bigger value will display \
+						the last frame. This property is useless \
+						when amplitude is ignored.',
+		default = 1.0,
+		update = set_maxi
+		)
+	
 	
--- single_track/CurveToFrameProperty.py
+++ single_track/CurveToFrameProperty.py
@@ -70,26 +70,6 @@ class CurveToFrameProperty():
 	
 	
 	
-	def set_mini(self, context):
-		'''check that maxi value are greater than maxi value 
-				when editing mini value'''
-		if self.mini > self.maxi:
-			self['maxi'] = self.mini
-	
-	
-	
-	
-	
-	def set_maxi(self, context):
-		'''check that maxi value are greater than maxi value
-				when editing maxi value'''
-		if self.mini > self.maxi:
-			self['mini'] = self.maxi
-	
-	
-	
-	
-	
 	def initialize( self ):
 		'''init or reload movieclip info'''
 		clip = self.id_data
@@ -435,75 +415,6 @@ class CurveToFrameProperty():
 	
 	size = bpy.props.IntProperty() # number of frame of the sequence
 	ext = bpy.props.StringProperty() # extension of source file
-	
-	#################################################
-	##     amplitude settings                      ##
-	#################################################
-	# amplitude property
-	amplitude = bpy.props.FloatProperty(
-		name = 'amplitude (raw)',
-		description = 'Determined the frame of the Movie \
-								clip to use at each frame',
-		default = 0.0
-		)
-	
-	# amplitude after applying min and max value
-	amplitude_net = bpy.props.FloatProperty(
-		name = 'amplitude (net)',
-		description = 'show the apply of mini and maxi to \
-							amplitude raw. Can\'t be edit.',
-		)
-	
-	# method used to combine amplitude and peaks curve
-	combination_mode = bpy.props.EnumProperty(
-		name = 'combination mode',
-		description = 'the way to combine amplitude and peaks curve',
-		default = 'ignore_peaks',
-		items = [
-#			(identifier,			name,
-#				description, number)
-			
-			('multiply',		'Peaks Curve Multiplied by amplitude',
-				'peaks is multiplied by \
-				amplitude percentage of maxi',				0),
-			
-			('clamp_key',		'Peaks Keyframe Clamped to amplitude',
-				'peaks keyframe is clamped by amplitude',		1),
-			
-			('clamp_curve',		'Peaks Curve Clamped to amplitude',
-				'all peaks value is clamped by amplitude',		2),
-			
-			('ignore_amplitude',			'Only use peaks curve',
-				'Only use peaks curve',			3),
-			
-			('ignore_peaks',			'Only use amplitude curve',
-				'Only use amplitude curve',			4)
-			
-			],
-		update = update_curves
-		)
-	
-	# min value associated to the first frames
-	mini = bpy.props.FloatProperty(
-		name = 'Mini',
-		description = 'the minimal value of the \
-						amplitude curve, all smaller\
-						 value will display the first frame',
-		default = 0.0,
-		update = set_mini
-		)
-	
-	# max value associated to the last frames
-	maxi = bpy.props.FloatProperty(
-		name = 'Maxi',
-		description = 'the maximal value of the amplitude\
-						 curve. All bigger value will display \
-						the last frame. This property is useless \
-						when amplitude is ignored.',
-		default = 1.0,
-		update = set_maxi
-		)
-	
 	
 	
 	
