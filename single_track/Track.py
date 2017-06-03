de4b49fbe8f8aa700790b171e12b503f08cb58df
Captain DesAstres <captaindesastres@gmail.com>
2017-06-03 14:41:55 +0200

    move properties and method to Track class

--- single_track/Track.py
+++ single_track/Track.py
@@ -1,4 +1,4 @@
-
+import bpy
 
 
 class Track():
@@ -6,4 +6,74 @@ class Track():
 		relative to the track info and settings for 
 		Curve To Frame addon'''
 	
+	def set_end_frame(self, context):
+		'''check that start and end frame are valid when 
+				changing end frame settings'''
+		# check end isn't over clip size
+		if self.end > self.size:
+			self.end = self.size
+		
+		# check start isn't over end
+		if self.start >= self.end:
+			if self.end > 1:
+				self['start'] = self.end - 1
+			else:
+				self['start'] = 1
+				self['end'] = 2
 	
+	
+	
+	
+	
+	def set_start_frame(self, context):
+		'''check that start and end frame are valid 
+				when changing start frame settings'''
+		# check start isn't under 0
+		if self.start < 1:
+			self.start = 1
+		
+		# check start isn't over end
+		if self.start >= self.end:
+			if self.start < self.size:
+				self['end'] = self.start + 1
+			else:
+				self['start'] = self.size - 1
+				self['end'] = self.size
+	
+	
+	
+	
+	# flag to know if curve to frame have been initialize on this MovieClip
+	init = bpy.props.BoolProperty(default = False)
+	uid = bpy.props.StringProperty( default = '' )
+	
+	#################################################
+	##     clip settings                           ##
+	#################################################
+	
+	path = bpy.props.StringProperty() # The sources directory path
+	prefix = bpy.props.StringProperty() # the source name prefix
+	suffix = bpy.props.StringProperty() # the source name suffix
+	number_size = bpy.props.IntProperty() # the source name frame number size in char
+	first = bpy.props.IntProperty() # the first frame number (in source file name)
+	last = bpy.props.IntProperty() # the last frame number (in source file name)
+	
+	# first frame of the clip to use
+	start = bpy.props.IntProperty(
+		name = "First frame",
+		description = "first frame that Frames Animated \
+					By Curve add-on must take in count",
+		default = 1,
+		min = 1,
+		update = set_start_frame)
+	
+	# last frame of the clip to use
+	end = bpy.props.IntProperty(
+		name = "Last frame",
+		description = "last frame that Frames Animated \
+					By Curve add-on must take in count",
+		update = set_end_frame)
+	
+	
+	size = bpy.props.IntProperty() # number of frame of the sequence
+	ext = bpy.props.StringProperty() # extension of source file
