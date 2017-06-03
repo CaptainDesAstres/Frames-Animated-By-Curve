570dd82119d4d826be3ed59c98ffa8bf524fea14
Captain DesAstres <captaindesastres@gmail.com>
2017-06-03 14:59:08 +0200

    move property and method to Combination class

--- single_track/Combination.py
+++ single_track/Combination.py
@@ -6,4 +6,37 @@ class Combination():
 		relative to combination settings for 
 		Curve To Frame addon'''
 	
+	def update_curves( self, context ):
+		'''method that must be over ride: update curve when settings have been changed'''
+	
+	
+	
+	# method used to combine amplitude and peaks curve
+	combination_mode = bpy.props.EnumProperty(
+		name = 'combination mode',
+		description = 'the way to combine amplitude and peaks curve',
+		default = 'ignore_peaks',
+		items = [
+#			(identifier,			name,
+#				description, number)
+			
+			('multiply',		'Peaks Curve Multiplied by amplitude',
+				'peaks is multiplied by \
+				amplitude percentage of maxi',				0),
+			
+			('clamp_key',		'Peaks Keyframe Clamped to amplitude',
+				'peaks keyframe is clamped by amplitude',		1),
+			
+			('clamp_curve',		'Peaks Curve Clamped to amplitude',
+				'all peaks value is clamped by amplitude',		2),
+			
+			('ignore_amplitude',			'Only use peaks curve',
+				'Only use peaks curve',			3),
+			
+			('ignore_peaks',			'Only use amplitude curve',
+				'Only use amplitude curve',			4)
+			
+			],
+		update = update_curves
+		)
 	
