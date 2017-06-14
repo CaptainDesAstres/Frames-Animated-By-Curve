9b2a837ae7e56e3d86a8d3ef11e4d4b01e15bc36
Captain DesAstres <captaindesastres@gmail.com>
2017-06-14 12:31:20 +0200

    create switch_mode property and update_switch_curve method in Switch class

--- multi_track/Switch.py
+++ multi_track/Switch.py
@@ -1,7 +1,32 @@
 from .SwitchMoment import SwitchMoment
+import bpy
 
 class Switch(SwitchMoment):
 	'''a class regrouping all settings and method
 			about track switching'''
 	
+	def update_switch_curve( self, context ):
+		'''update switching curve'''
+		return
 	
+	
+	
+	
+	####################
+	##   Properties   ##
+	####################
+	
+	switch_mode = bpy.props.EnumProperty(
+		name = 'Switch mode',
+		description = 'Define how to switch between tracks.',
+		default = 'manual',
+		items = [
+#			(identifier,			name,
+#				description, number)
+			
+			('manual',		'Manually',
+				'You set tracks manually',				0)
+			
+			],
+		update = update_switch_curve
+		)
