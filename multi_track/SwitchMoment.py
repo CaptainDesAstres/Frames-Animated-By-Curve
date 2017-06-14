8b0e3f03a83b850dbdd61610c1c5c22dc8528a07
Captain DesAstres <captaindesastres@gmail.com>
2017-06-14 13:01:22 +0200

    add switch_at_perfect_frame property

--- multi_track/SwitchMoment.py
+++ multi_track/SwitchMoment.py
@@ -1,5 +1,23 @@
+import bpy
+
 class SwitchMoment:
 	'''a class regrouping all settings and method
 			about track switching'''
+	
+	def update_switch_curve( self, context ):
+		'''update switching curve. must be rewrite in Switch class.'''
+		return
+	
+	
+	####################
+	##   Properties   ##
+	####################
+	
+	switch_at_perfect_frame = bpy.props.BoolProperty(
+		name = 'Frame perfect',
+		description = 'Switch at the exact frame where you decide.',
+		default = True,
+		update = update_switch_curve
+		)
 	
 	
