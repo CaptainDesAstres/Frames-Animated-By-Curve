79f17781e7512cc35ce23c85e8fb7c1024358cb2
Captain DesAstres <captaindesastres@gmail.com>
2017-06-14 14:01:37 +0200

    create scene «animation data» if none

--- multi_track/Switch.py
+++ multi_track/Switch.py
@@ -7,6 +7,14 @@ class Switch(SwitchMoment):
 	
 	def update_switch_curve( self, context ):
 		'''update switching curve'''
+		ob = self.id_data
+		if ob.animation_data is None:
+			ob.animation_data_create()
+		
+		if ob.animation_data.action is None:
+			ob.animation_data.action = bpy.data.actions.new( 
+						name= ob.name+'Action')
+		
 		self.update_switch_keyframe_curve()
 		
 	
