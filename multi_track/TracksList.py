c1c86201c0e2e32719b9f5be9a676b86fb66633d
Captain DesAstres <captaindesastres@gmail.com>
2017-06-19 14:18:36 +0200

    avoid unvalid start/end value while setting

--- multi_track/TracksList.py
+++ multi_track/TracksList.py
@@ -4,12 +4,51 @@ from functions import *
 
 class Track(bpy.types.PropertyGroup):
 	'''object use to be listed as track in tracks list'''
+	
+	def set_end_frame(self, context):
+		'''check that start and end frame are valid when 
+				changing end frame settings'''
+		size = self.get(True).curve_to_frame.size
+		# check end isn't over clip size
+		if self.end > size:
+			self.end = size
+		
+		# check start isn't over end
+		if self.start >= self.end:
+			if self.end > 1:
+				self['start'] = self.end - 1
+			else:
+				self['start'] = 1
+				self['end'] = 2
+	
+	
+	
+	
+	
+	def set_start_frame(self, context):
+		'''check that start and end frame are valid 
+				when changing start frame settings'''
+		# check start isn't under 0
+		if self.start < 1:
+			self.start = 1
+		
+		# check start isn't over end
+		if self.start >= self.end:
+			size = self.get(True).curve_to_frame.size
+			if self.start < size:
+				self['end'] = self.start + 1
+			else:
+				self['start'] = size - 1
+				self['end'] = size
+	
+	
+	
 	# all properties
 	name = bpy.props.StringProperty()
 	uid = bpy.props.StringProperty()
 	track_id = bpy.props.IntProperty()
-	start = bpy.props.IntProperty()
-	end = bpy.props.IntProperty()
+	start = bpy.props.IntProperty(update = set_start_frame)
+	end = bpy.props.IntProperty(update = set_end_frame)
 	
 	def get( self, rename = False):
 		'''return the movie clip corresponding to this track'''
